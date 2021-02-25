00a1aa7a923574e381e86b3ce09b0814a89758f7
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-02-25 14:13:29 -0600

    writing queries for select exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,5 +1,8 @@
 USE codeup_test_db;
 
-SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT name, release_date FROM albums WHERE release_date >= 1990 && release_date <= 2000;
+SELECT * FROM albums WHERE sales < 20;
+SELECT * FROM albums WHERE genre = 'Rock';
