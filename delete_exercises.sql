724dfac05baeb704b5aae50d9c9af5ee09c3de23
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-02-25 14:36:48 -0600

    finished delete exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,7 +1,6 @@
 USE codeup_test_db;
 
 DELETE FROM albums WHERE release_date > 1991;
-
 DELETE FROM albums WHERE genre = 'Disco';
-
 DELETE FROM albums WHERE artist = 'Metallica';
+SELECT * FROM albums;
