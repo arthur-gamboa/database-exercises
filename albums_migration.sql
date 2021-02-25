f6c200f8b5912cb16fb13f5e46bb921ef4225c78
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-02-24 19:23:49 -0600

    added albums table to console

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+# USE codeup_test_db;
+# CREATE TABLE albums (
+#     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#     artist VARCHAR(50),
+#     name VARCHAR(50),
+#     release_date INT(4),
+#     sales FLOAT(50),
+#     genre VARCHAR(50),
+#     PRIMARY KEY (id)
+# );
