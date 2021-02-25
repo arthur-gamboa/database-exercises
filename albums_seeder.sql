234cf3195742408a54456c92f8856f58c23c1d63
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-02-25 11:35:03 -0600

    added album values

---
+++ albums_seeder.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 47.3, 'Pop'),
+('AC/DC', 'Back In Black', 1980, 29.4, 'Hard Rock'),
+('Pink Floyd', 'The Dark Side of the Moon', 1977, 21.7 ,'Hard Rock'),
+('Whitney Houston', 'The Bodyguard', 1992, 28.4, 'R and B'),
+('Eagles', 'Their Greatest Hits (1971 - 1975)', 1976, 41.2, 'Country Rock'),
+('Bee Gees', 'Saturday Night Fever', 1977, 21.6, 'Disco'),
+('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft Rock'),
+('Shania Twain', 'Come On Over', 1997, 29.6, 'Country');
