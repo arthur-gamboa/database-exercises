8a7b037b3278b698a0c9477ca2c8eb2096110d22
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-02-26 16:21:05 -0600

    finished group by exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,9 +1,43 @@
-USE employees
+USE employees;
 
-SELECT count(*) AS 'number_of_employees' FROM employees;
+# LECTURE EXERCISES
+# SELECT count(*) AS 'number_of_employees' FROM employees;
+#
+# SELECT first_name, COUNT(first_name) AS TOTAL
+# FROM employees
+# GROUP BY first_name DESC;
+#
+# SELECT last_name, first_name, COUNT(first_name) AS 'total'
+# FROM employees
+# GROUP BY last_name, first_name
+# ORDER BY total DESC;
+#
+# SELECT * FROM employees WHERE first_name = 'Laurentiu' AND last_name = 'Cesareni';
 
-SELECT first_name, COUNT(first_name)
+SELECT DISTINCT title
+FROM titles;
+
+SELECT DISTINCT last_name
 FROM employees
-GROUP BY first_name DESC;
+WHERE last_name LIKE 'E%e'
+GROUP BY last_name ASC;
 
-SELECT * FROM employees WHERE first_name = 'Georgi';
+SELECT DISTINCT last_name, first_name
+FROM employees
+WHERE last_name LIKE 'E%e'
+GROUP BY last_name ASC, first_name ASC;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND NOT last_name LIKE '%qu%';
+
+SELECT COUNT(*)
+FROM employees
+WHERE last_name LIKE '%q%'
+AND NOT last_name LIKE '%qu%';
+
+SELECT COUNT(*)
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
