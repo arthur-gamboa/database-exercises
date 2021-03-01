fa6fa32a98ad73aee7cd9e83a1ef528bf280836d
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-03-01 14:07:44 -0600

    left join user/roles

--- join_exercises.sql
+++ join_exercises.sql
@@ -38,8 +38,17 @@ SHOW TABLES;
 DESCRIBE users;
 DESCRIBE roles;
 
---
+-- Join / Inner Join
 
 SELECT users.name as user_name, roles.name as role_name
 FROM users
 JOIN roles ON users.role_id = roles.id;
+
+-- Left Join
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+LEFT JOIN roles ON users.role_id = roles.id;
+
+-- Right Join
+
